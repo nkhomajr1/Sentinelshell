8b70de6dfdb1a5ad6d51ce38944643b4ceaa774c
nkhomajr1 <wilsonmkango@gmail.com>
2025-06-18 23:19:00 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "[+] Deploying SentinelShell to GitHub..."
+
+git init
+git remote add origin https://github.com/nkhoma1/SentinelShell.git
+git add .
+git commit -m "🚀 Initial release of SentinelShell"
+git branch -M main
+git push -u origin main
+
+echo "[✔] SentinelShell is now live at:"
+echo "https://github.com/nkhoma1/SentinelShell"
