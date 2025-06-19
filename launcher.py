abdb4793de53ff674a8b6b9fad532422d9af6e0f
nkhomajr1 <wilsonmkango@gmail.com>
2025-06-18 23:03:21 -0700

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -3,6 +3,7 @@ import sys, os
 print("sys.path at start:", sys.path)
 print("Current working directory:", is.getcwd())
 from core import scanner
+scanner.scan_directory("test-folder")
 from core.restorer import restore_file
 from utils.theme import enable_ansi, banner
 from utils.rain import matrix_rain
